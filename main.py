a7c9d177be4f46b75ced6795f0ae6ac53f888ea0
unknown <hopeing02@gmail.com>
2026-01-02 08:55:26 +0900

    froend commit

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 from api import generate, test, fix, deploy
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
 app = FastAPI(title="gas ai platform")
+
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+@app.get("/")
+def root():
+    return FileResponse("frontend/index.html")
+
 app.include_router(generate.router)
 app.include_router(test.router)
 app.include_router(fix.router)
