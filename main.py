31aaf9eece937039487593e70276f8f38809d063
unknown <hopeing02@gmail.com>
2026-01-02 09:33:43 +0900

    app commit

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ app = FastAPI(title="gas ai platform")
 
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
-@app.get("/")
-def root():
-    return FileResponse("frontend/index.html")
-
 app.include_router(generate.router)
 app.include_router(test.router)
 app.include_router(fix.router)
 app.include_router(deploy.router)
+
+@app.get("/")
+def root():
+    return FileResponse("frontend/index.html")
