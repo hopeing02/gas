f563326b740df042cb6533f10699f9e8a5597200
unknown <hopeing02@gmail.com>
2026-01-02 09:40:31 +0900

    app2 commit

--- Dockerfile.dockerfile
+++ Dockerfile.dockerfile
@@ -3,7 +3,7 @@ FROM python:3.11-slim
 RUN apt-get update && apt-get install -y nodejs npm
 RUN npm install -g @google/clasp
 
-WORKDIR /
+WORKDIR /app
 COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
--- api/test.py
+++ api/test.py
@@ -1,25 +1,6 @@
-<<<<<<< HEAD
-import subprocess
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.post("/test")
-def test():
-    result = subprocess.run(
-        ["clasp", "push"],
-        capture_output=True,
-        text=True
-    )
-    return {
-        "stdout": result.stdout,
-        "stderr": result.stderr
-    }
-=======
 from fastapi import APIRouter
 from services.execution_service import run_test
 router = APIRouter(prefix="/test")
 @router.post("")
 def test():
     return run_test()
->>>>>>> 9ccf4c3b5649425d0074fe8304f4021b0bd1c5ba
