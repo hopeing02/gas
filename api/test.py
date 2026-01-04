6e2c0e2e336c879e7087c8833828e71e36a7bd11
hopeing02 <hopeing02@gmail.com>
2026-01-04 20:22:42 +0900

    Update test.py

--- api/test.py
+++ api/test.py
@@ -1,6 +1,27 @@
-from fastapi import APIRouter
-from services.execution_service import run_test
-router = APIRouter(prefix="/test")
-@router.post("")
-def test():
-    return run_test()
+from fastapi import APIRouter, HTTPException
+from services.clasp_service import clasp_push, clasp_run_test
+
+router = APIRouter()
+
+@router.post("/test")
+def test_gas():
+    try:
+        clasp_push()
+        result = clasp_run_test()
+
+        if result == "OK":
+            return {
+                "status": "success",
+                "result": result
+            }
+
+        return {
+            "status": "fail",
+            "result": result
+        }
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
