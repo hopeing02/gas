02a67bd5f8319adfd93ad295892c9e0a32e9e986
hopeing02 <hopeing02@gmail.com>
2026-01-05 13:41:55 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,10 @@ mkdir -p /root
 echo "$CLASPRC_JSON" > /root/.clasprc.json
 chmod 600 /root/.clasprc.json
 
+# 디버그 (한번만 확인용)
+which clasp
+clasp --version
+
 # 서버 실행
 uvicorn main:app --host 0.0.0.0 --port ${PORT:-8000}
 
